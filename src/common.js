dcb625edd07fea890014d2e353b013a38fcd326c
Niko <nik101skryto@gmail.com>
2020-02-24 21:55:19 +0400

    Сделал Colors&Type и начал делать Cards.
    Доработал Colors&Type.

---
+++ src/common.js
@@ -0,0 +1,8 @@
+Test = () => {
+    console.log("DDDDDDD")
+}
+
+var a, b;
+a = 40;
+b = 20;
+console.log(Test(), a + b + '\n' + "Единиц")
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './common.js'
+import './main.scss'
+import './pug/block/colorRendering/colorRendering.scss'
+import './pug/mixin/mixin-titleBlock-textBlock/mixin-titleBlock-textBlock.scss'
